6612f6b391e0f4d253fa16707022d449c3ac0f84
rajuraj1994 <rajurajkandel@gmail.com>
2021-05-02 11:53:54 +0545

    fourth commit

---
+++ formvalidation.js
@@ -0,0 +1,77 @@
+function fnameValidate(){
+    let first_name=document.getElementById('fname').value
+    if(first_name==""){
+        displayMsg('firstname is required','fnamemsg','red')
+        return false
+    }
+    else if(first_name.length<4){
+        displayMsg('firstname must be more than 3 characters','fnamemsg','red')
+        return false;
+    }
+    else if(!first_name.match(/^([a-zA-Z])+$/)){
+        displayMsg('firstname contains only alphabets','fnamemsg','red')
+        return false
+
+    }
+
+    else{
+        displayMsg('valid firstname','fnamemsg','green')
+        return true
+    }
+}
+
+function lnameValidate(){
+    let last_name=document.getElementById('lname').value
+    if(last_name==""){
+        displayMsg('lastname is required','lnamemsg','red')
+        return false
+    }
+    else if(last_name.length<3){
+        displayMsg('lastname must be more than 2 characters','lnamemsg','red')
+        return false
+    }
+    else{
+        displayMsg('valid lastname','lnamemsg','green')
+        return true
+    }
+}
+
+function emailValidate(){
+    let email=document.getElementById('email').value
+    if(email==""){
+        displayMsg('email is required','emailmsg','red')
+        return false
+    }
+    
+    else{
+        displayMsg('valid email','emailmsg','green')
+        return true
+    }
+}
+
+function passValidate(){
+    let pass=document.getElementById('pass').value
+    if(pass==""){
+        displayMsg('password is required','passmsg','red')
+        return false
+    }
+    else if(pass.length<9){
+        displayMsg('password must be more than 8 characters','passmsg','red')
+        return false
+    }
+    else{
+        displayMsg('valid password','passmsg','green')
+        return true
+    }
+}
+
+
+
+
+function displayMsg(msg,id,color){
+document.getElementById(id).innerHTML=msg
+document.getElementById(id).style.color=color
+
+
+
+}
